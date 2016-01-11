ffedf771a50e4dfef9af9f15b180d8602f1b7764
Juan Sidrach de Cardona Mora <jsidrach@ucsd.edu>
2016-01-10 22:45:20 -0800

    Jan. 10, 2016

---
+++ Algorithms-JavaScript/154-Find-Minimum-in-Rotated-Sorted-Array-II.js
@@ -0,0 +1,41 @@
+/*
+Follow up for "Find Minimum in Rotated Sorted Array":
+What if duplicates are allowed?
+
+Would this affect the run-time complexity? How and why?
+Suppose a sorted array is rotated at some pivot unknown to you beforehand.
+
+(i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+
+Find the minimum element.
+
+The array may contain duplicates.
+*/
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMin = function(nums) {
+  'use strict';
+
+  let lower = 0;
+  let upper = nums.length - 1;
+
+  while (lower < upper) {
+    const middle = lower + Math.floor((upper - lower) / 2);
+    const num = nums[middle];
+
+    if (num > nums[upper]) {
+      lower = middle + 1;
+    }
+    else if (num < nums[lower]) {
+      upper = middle;
+    }
+    else {
+      --upper;
+    }
+  }
+
+  return nums[lower];
+};
---
+++ Algorithms-JavaScript/73-Set-Matrix-Zeroes.js
@@ -0,0 +1,64 @@
+/*
+Given a m x n matrix, if an element is 0, set its entire row and column to 0. Do it in place.
+
+Follow up:
+Did you use extra space?
+A straight forward solution using O(mn) space is probably a bad idea.
+A simple improvement uses O(m + n) space, but still not the best solution.
+Could you devise a constant space solution?
+*/
+
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var setZeroes = function(matrix) {
+  'use strict';
+
+  const rows = matrix.length;
+  if (rows === 0) {
+    return;
+  }
+
+  const columns = matrix[0].length;
+
+  let firstRowZero = false;
+  let firstColZero = false;
+
+  for (let i = 0; i < rows; ++i) {
+    for (let j = 0; j < columns; ++j) {
+      if (matrix[i][j] === 0) {
+        if (i === 0) {
+          firstRowZero = true;
+        }
+
+        if (j === 0) {
+          firstColZero = true;
+        }
+
+        matrix[0][j] = 0;
+        matrix[i][0] = 0;
+      }
+    }
+  }
+
+  for (let i = 1; i < rows; ++i) {
+    for (let j = 1; j < columns; ++j) {
+      if (matrix[0][j] === 0 || (matrix[i][0] === 0)) {
+        matrix[i][j] = 0;
+      }
+    }
+  }
+
+  if (firstRowZero) {
+    for (let j = 0; j < columns; ++j) {
+      matrix[0][j] = 0;
+    }
+  }
+
+  if (firstColZero) {
+    for (let i = 0; i < rows; ++i) {
+      matrix[i][0] = 0;
+    }
+  }
+};
