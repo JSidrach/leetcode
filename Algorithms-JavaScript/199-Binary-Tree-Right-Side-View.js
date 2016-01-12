a61dc6edb4eab01de717561b614d9b0ac07d3cb2
Juan Sidrach de Cardona Mora <jsidrach@ucsd.edu>
2016-01-11 20:03:09 -0800

    Jan. 11, 2016

---
+++ Algorithms-JavaScript/199-Binary-Tree-Right-Side-View.js
@@ -0,0 +1,55 @@
+/*
+Given a binary tree, imagine yourself standing on the right side of it, return the values of the nodes you can see ordered from top to bottom.
+
+For example:
+Given the following binary tree,
+   1            <---
+ /   \
+2     3         <---
+ \     \
+  5     4       <---
+You should return [1, 3, 4].
+*/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var rightSideView = function(root) {
+  'use strict';
+
+  if (root === null) {
+    return [];
+  }
+
+  let output = [];
+  let list = [root];
+  let listLength = 1;
+
+  while (listLength !== 0) {
+    output.push(list[listLength - 1].val);
+
+    for (let i = 0; i < listLength; ++i) {
+      const node = list.shift();
+
+      if (node.left !== null) {
+        list.push(node.left);
+      }
+
+      if (node.right !== null) {
+        list.push(node.right);
+      }
+    }
+
+    listLength = list.length;
+  }
+
+  return output;
+};
